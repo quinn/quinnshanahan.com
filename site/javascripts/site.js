d815964f674f5720c6ffb0726008d79482526646
Quinn Shanahan <quinn.shanahan@controlgroup.com>
2010-08-17 13:13:56 -0400

    added flickr

--- site/javascripts/site.js
+++ site/javascripts/site.js
@@ -1,12 +1,15 @@
-jQuery.makeContainer = function makeContainer (name, url, renderer) {
+jQuery.makeContainer = function makeContainer (name, url, opts) {
   var container = $('#areas > .' + name);
   var template = container.find('.row-template');
+  var renderer = opts.renderer || opts;
+  var processor = opts.processor || function (d) { return d; };
 
   $.getJSON(url, function (data) {
     container.find('.loading').hide();
 
     var i = 0;
-    $.each(data, function () {
+    var set = processor(data);
+    $.each(set, function () {
       var row = template.clone()
         .appendTo(container)
         .addClass('row')
@@ -26,82 +29,92 @@ jQuery(function ($) {
   var github = "http://github.com/quinn.json?callback=?";
   var twitter = "http://twitter.com/status/user_timeline/dontdie.json?count=10&callback=?";
   var tumblr  = "http://quinn.tumblr.com/api/read/json?callback=?";
+  var flickr  = "http://api.flickr.com/services/feeds/photos_public.gne?id=57195921@N00&format=json&jsoncallback=?";
 
-  $.makeContainer('github', github, function (container, row) {
-    var payload = this.payload;
-    var repo = this.repository;
-    var url = "";
-    var linkText = "";
-    var description = "";
+  $.makeContainer('github', github, {
+    processor: function (data) {
+      return $.map(data, function (item) {
+        var payload = item.payload;
+        var repo = item.repository;
+        var url = "";
+        var linkText = "";
+        var description = "";
 
-    if (repo) description = repo.description;
-    if (this) url = this.url;
-    if (!url && payload) url = payload.url;
-    if (!url && payload) url = "http://github.com/" + payload.target;
+        if (repo) description = repo.description;
+        if (item) url = item.url;
+        if (!url && payload) url = payload.url;
+        if (!url && payload) url = "http://github.com/" + payload.target;
 
-    switch(this.type) {
-    case "FollowEvent":
-      linkText = "started following " + payload.target;
-      break;
+        switch(item.type) {
+        case "FollowEvent":
+          linkText = "started following " + payload.target;
+          break;
 
-    case "GistEvent":
-      linkText = payload.action + "d " + payload.name;
-      description = payload.snippet;
-      break;
+        case "GistEvent":
+          linkText = payload.action + "d " + payload.name;
+          description = payload.snippet;
+          break;
 
-    case "PushEvent":
-      if (repo) linkText = "pushed to " + repo.name;
-      description = $(payload.shas).last()[0][2];
-      break;
+        case "PushEvent":
+          if (repo) linkText = "pushed to " + repo.name;
+          description = $(payload.shas).last()[0][2];
+          break;
 
-    case "CreateEvent":
-      if (repo) linkText = "created " + repo.name;
-      break;
+        case "CreateEvent":
+          if (repo) linkText = "created " + repo.name;
+          break;
 
-    case "WatchEvent":
-      linkText = payload.action + " watching " + repo.name;
-      break;
+        case "WatchEvent":
+          linkText = payload.action + " watching " + repo.name;
+          break;
 
-    case "CommitCommentEvent":
-      linkText = "commented on " + repo.name + " by " + repo.owner;
-      break;
+        case "CommitCommentEvent":
+          linkText = "commented on " + repo.name + " by " + repo.owner;
+          break;
 
-    case "ForkEvent":
-      linkText = "forked " + repo.name + " by " + repo.owner;
-      break;
+        case "ForkEvent":
+          linkText = "forked " + repo.name + " by " + repo.owner;
+          break;
 
-    case "MemberEvent":
-      linkText = payload.action + " " + repo.name;
-      break;
+        case "MemberEvent":
+          linkText = payload.action + " " + repo.name;
+          break;
 
-    default:
-      linkText = this.type + ' on github';
-    }
+        default:
+          linkText = item.type + ' on github';
+        }
 
-    if (url != "") {
-      row
-        .find('a')
-          .attr('href', url)
-          .html(linkText)
-          .end()
-        .find('.description')
-          .html(description)
-          .end();
-      return true;
-    } else {
-      return false;
+        item.url = url;
+        item.linkText = linkText;
+        item.description = description;
+        return item;
+      });
+    },
+    renderer: function (container, row) {
+      if (this.url != "") {
+        row
+          .find('a')
+            .attr('href', this.url)
+            .html(this.linkText)
+            .end()
+          .find('.description')
+            .html(this.description)
+            .end();
+        return true;
+      } else {
+        return false;
+      }
     }
   });
 
   $.makeContainer('twitter', twitter, function (container, row) {
-    row
+    return row
       .find('a')
         .attr('href', "http://twitter.com/dontdie/status/" + this.id)
         .end()
       .find('.description')
         .html(this.text)
         .end();
-    return true;
   });
 
   $.makeContainer('tumblr', tumblr, {
@@ -109,14 +122,21 @@ jQuery(function ($) {
       return data.posts;
     },
     renderer: function (container, row) {
-      row
-        .find('a')
-          .attr('href', "http://twitter.com/dontdie/status/" + this.id)
-          .end()
-        .find('.description')
-          .html(this.text)
-          .end();
       return true;
+    }
+  });
+
+  $.makeContainer('flickr', flickr, {
+    processor: function (data) {
+      return $.map(data.items, function (item) {
+        item.media.s = item.media.m.replace("_m.jpg", "_s.jpg");
+        return item;
+      });
+    },
+    renderer: function (container, row) {
+      return row
+        .find('img')
+          .attr('src', this.media.s);
     }
   });
 });
